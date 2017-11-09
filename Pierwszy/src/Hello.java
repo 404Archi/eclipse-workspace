7675f68db0ef0f6001b6378be2f77a0e72cdfa56
Polomij <31891495+Polomij@users.noreply.github.com>
2017-11-09 15:23:42 +0100

    09,11

--- Pierwszy/src/Hello.java
+++ Pierwszy/src/Hello.java
@@ -6,17 +6,17 @@ public class Hello
 	{
 		int sum=0;
 		
-		for(int i=1; i<=10; i++)
+		for(int i=0; i<=10; i++)
 		{
 			sum= sum+i;
-			System.out.print(sum);
-			System.out.print(" ");
-			System.out.println(i);
+			System.out.print(i);
+			System.out.print(": ");
+			System.out.println(sum);
 		}
 		System.out.print("Ostateczny wynik:");
 		System.out.println(sum);
 		
-		System.out.println(Integer.MAX_VALUE);
-		System.out.println(Integer.MIN_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+//		System.out.println(Integer.MIN_VALUE);
 	}
 }
