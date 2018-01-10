17da6f321673d51e52682f5fe54203358b0db716
Polomij <31891495+Polomij@users.noreply.github.com>
2018-01-10 17:16:26 +0100

    1

--- Matura/src/Tablica_T.java
+++ Matura/src/Tablica_T.java
@@ -3,27 +3,82 @@ import java.util.Scanner;
 public class Tablica_T 
 {
 	static Scanner s = new Scanner(System.in);
-	public static void main(String[] args) 
+	static int k;
+	static int n;
+
+	public static void ilosc()
 	{
 		System.out.print("Ile chcesz liczb\n");
-		int k = s.nextInt();
-		int tab[]=new int[k];
+		k = s.nextInt();
+	}
+	
+	public static void liczby(int[] tab)
+	{
 		for (int i = 0; i < k;i++)
 		{
 			System.out.print("podaj liczbe \n");
 			tab[i]=s.nextInt();
 		}
-		int zlicz[]=new int[tab.length];
-		for (int i = 0; i < tab.length-1;i++)
+	}
+	
+	public static int max (int[] tab)
+	{
+		int max = Integer.MIN_VALUE;
+
+		for(int i = 0;i<k;i++)
 		{
-			
-			zlicz[tab[i]]=zlicz[tab[i]]+1;
-		}
-		for(int i=0;i<tab.length;i++)
+			if((tab[i] > max)&&(tab[i]!=0))
+			{
+				max = tab[i];
+
+			}
+		}		
+		return max;
+	}
+
+	public static void wypisz(int[] zlicz)
+	{
+		for(int i=0;i<n;i++)
 		{
 			System.out.print(zlicz[i]+", ");
 		}
+	}
+	
+	public static void zeruj(int max,int Z[])
+	{
+		for (int i = 0; i < max;i++)
+		{
+			Z[i]=0;
+		}
+	}
+	
+	public static void n()
+	{
+		for (int i = 0; i <= k-1;i++)
+		{
+			n++;
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		ilosc();
+		int T[]=new int[k];
+		liczby(T);
+		max(T);
+		int Z[]=new int[k];
+		zeruj(k,Z);
+		n();
 
+		
+		for (int i = 0; i < n;i++)
+		{
+			
+			Z[T[i]]=Z[T[i]]+1;
+		}
+		wypisz(Z);
+	
+	 
 	}
 
 }
